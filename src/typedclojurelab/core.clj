e88d2c9205f3bd7c16fb3a10c08d4eee87ef1e3b
Martin Jul <martin@mjul.com>
2015-03-27 21:56:04 +0100

    Add template project.

---
+++ src/typedclojurelab/core.clj
@@ -0,0 +1,6 @@
+(ns typedclojurelab.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/typedclojurelab/core_test.clj
@@ -0,0 +1,7 @@
+(ns typedclojurelab.core-test
+  (:require [clojure.test :refer :all]
+            [typedclojurelab.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
